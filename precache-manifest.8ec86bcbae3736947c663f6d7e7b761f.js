a232541bf3e27185f2d81c620ab7b4e5559c13fe
asd <asd>
2018-12-18 00:35:25 +0300

    Updates

---
+++ precache-manifest.8ec86bcbae3736947c663f6d7e7b761f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8a708111fa7c7dd9851",
+    "url": "/itra7/static/js/runtime~main.e8a70811.js"
+  },
+  {
+    "revision": "8e119afd8f9d62eb79df",
+    "url": "/itra7/static/js/main.8e119afd.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/itra7/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "8e119afd8f9d62eb79df",
+    "url": "/itra7/static/css/main.2ce658fd.chunk.css"
+  },
+  {
+    "revision": "306589e7c016a406f8d91c8aedaf4d45",
+    "url": "/itra7/index.html"
+  }
+];
